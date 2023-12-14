15035e170c300951c39e189f26da7c731d47008d
pppkkp123 <144075587+pppkkp123@users.noreply.github.com>
2023-12-14 15:14:24 +0800

    Create main2.c
    main2.c

---
+++ EX2/main2.c
@@ -0,0 +1,18 @@
+int main()
+{
+    
+   int i=30;
+   if(i%15==0){
+       printf("Love IU");
+   }
+   else if(i%3==0){
+       printf("Love");
+   }
+   else if(i%5==0){
+       printf("IU");
+   }
+   else{
+       printf("%d",i);
+   }
+    return 0;
+}
